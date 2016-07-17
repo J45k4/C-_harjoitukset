fa2e624c3e54d29e75280b1aa934b66b3252b3eb
Invaeskimo <pusanteemu@outlook.com>
2016-07-17 16:21:50 +0300

    Harjoitus 3 tehty

---
+++ Harjoitus3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> luvut = new List<int>();
+            Tehtava1(luvut);
+            Tehtava1(luvut);
+            for (int i = 0; i < luvut.Count; i++)
+            {
+                Console.WriteLine(luvut[i]);
+            }
+            int luku = 5;
+            Tehtava2(luku);
+            Console.WriteLine(luku);
+            Console.ReadKey();
+        }
+
+        static public void Tehtava1(List<int> luvut)
+        {
+            Random rand = new Random();
+            luvut.Add(rand.Next(1, 10));
+        }
+
+        static public void Tehtava2(int luku)
+        {
+            luku *= 2;
+        }
+    }
+}
